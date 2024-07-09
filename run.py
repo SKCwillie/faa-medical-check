117b5c096b7261263d7de70ab21073f3c4cfd965
Robert Williams <rwilliams651@gmail.com>
2024-07-08 23:36:48 -0500

    readding idea folder

--- run.py
+++ run.py
@@ -5,11 +5,13 @@ from functions import login, parse_html
 if __name__ == '__main__':
     email = os.getenv('EMAIL')
     password = os.getenv('PASSWORD')
+    sleep = os.getenv('SLEEP')
     while True:
         try:
             html = login(email, password)
             parse_html(html)
-            time.sleep(30 * 60)
+            time.sleep(SLEEP * 60)
         except:
             print('Error running automation . . . ')
             print('Trying again later')
+            time.sleep(SLEEP * 60)
