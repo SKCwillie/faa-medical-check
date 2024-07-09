a1d0ad854d783eeb7b716ee8fa58dc266d31bd13
Robert Williams <rwilliams651@gmail.com>
2024-07-08 23:50:22 -0500

    adding dotenv

--- run.py
+++ run.py
@@ -1,17 +1,19 @@
 import os
 import time
+from dotenv import load_dotenv
 from functions import login, parse_html
 
 if __name__ == '__main__':
+    load_dotenv()
     email = os.getenv('EMAIL')
     password = os.getenv('PASSWORD')
-    sleep = os.getenv('SLEEP')
+
     while True:
         try:
             html = login(email, password)
             parse_html(html)
-            time.sleep(SLEEP * 60)
+            time.sleep(30 * 60)
         except:
             print('Error running automation . . . ')
             print('Trying again later')
-            time.sleep(SLEEP * 60)
+            time.sleep(30 * 60)
